16859b8ca3ee9101a5da27dcd4cc1e6106e2f88c
Piolo-Ambion <pafambion@gmail.com>
2023-08-08 23:35:37 +0800

    Movement
    Added WASD movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] 
+    private float moveSpeed = 5f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
